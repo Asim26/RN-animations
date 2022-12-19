dba0acf6c4c35d69a513be1679e59a63cbb25ed3
asim26 <asimmehmood.codingpixel@gmail.com>
2022-12-19 15:16:39 +0500

    remove item animation

--- App.js
+++ App.js
@@ -1,46 +1,36 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import Animated ,{useAnimatedStyle, useSharedValue, withTiming, withSpring, useAnimatedGestureHandler} from 'react-native-reanimated'
-import {PanGestureHandler} from 'react-native-gesture-handler'
 
 const App =()=> {
-  const progress = useSharedValue(0);
-  const isPanning = useSharedValue(false);
+  const translateX = useSharedValue(0);
+  const removing = useSharedValue(false);
 
+  const handleRemove = () =>{
+    removing.value = true
+  }
   
-  const gestureHandler = useAnimatedGestureHandler({
-    onStart: (event,context) =>{
-      console.log('Animation started...');
-      context.startX = progress.value;
-    },
-    onActive: event =>{
-      console.log('Animation is active...');
-      isPanning.value=true;
-      progress.value = event.translationX;
-    },
-    onEnd: (event, context)=>{
-      console.log('Animation is ended...');
-      isPanning.value=false;
-      progress.value=withSpring(0,{
-        velocity:event.velocityX,
-      })
-    }
-  })
   
   const animatedStyles = useAnimatedStyle(()=>{
-    return{
-      backgroundColor: isPanning.value?'red':'green',
-      opacity: isPanning.value?0.12:1,
-      scale: withSpring(isPanning.value? 4 : 2 ),
-      transform:[{translateX:progress.value}]
+    if(removing.value){
+      return{
+        height: withTiming(0),
+        transform:[{translateY: withTiming(-40)}]
+      }  
     }
+      return{
+        height: withTiming(100),
+        transform:[{translateX: translateX.value}]
+      }
+    
   })
 
   return (
     <View style={styles.container}>
-      <PanGestureHandler onGestureEvent={gestureHandler}>
       <Animated.View style={[styles.box,animatedStyles]}/>
-      </PanGestureHandler>
+      <TouchableOpacity onPress={()=>{handleRemove()}}>
+        <Text>remove</Text>
+      </TouchableOpacity>
     </View>
   )
 }
