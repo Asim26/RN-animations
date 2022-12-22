7c29e864626ad327054662b31162bf0f2e67b49e
asimmehmood.codingpixel11 <asim.mehmood@codingpixel.com>
2022-12-22 10:55:56 +0500

    layout animations on flatlist

--- App.js
+++ App.js
@@ -1,16 +1,95 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import Ring from './components/Ring';
+import React, {useState} from 'react';
+import {StyleSheet, View, FlatList} from 'react-native';
+import Animated, {
+  BounceInRight,
+  BounceInLeft,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 const App = () => {
-  let duration = 4000;
+  const [data, setData] = useState([
+    {
+      id: '1',
+      title: 'First Item',
+    },
+    {
+      id: '2',
+      title: 'Second Item',
+    },
+    {
+      id: '3',
+      title: 'Third Item',
+    },
+    {
+      id: '4',
+      title: 'Fourth Item',
+    },
+    {
+      id: '5',
+      title: 'Fifth Item',
+    },
+    {
+      id: '6',
+      title: 'Sixth Item',
+    },
+    {
+      id: '7',
+      title: 'Seventh Item',
+    },
+    {
+      id: '8',
+      title: 'Eighth Item',
+    },
+    {
+      id: '9',
+      title: 'Ninth Item',
+    },
+    {
+      id: '10',
+      title: 'Tenth Item',
+    },
+  ]);
+
+  const checkOddEven = number => {
+    if (number % 2 == 0) {
+      return 'even';
+    } else {
+      return 'odd';
+    }
+  };
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: 'green',
+      height: 100,
+      marginVertical: 5,
+      marginHorizontal: 5,
+      borderRadius: 20,
+    };
+  });
+
+  const renderItem = (index, item) => {
+    checkOddEven(index);
+    return (
+      <Animated.View
+        style={animatedStyle}
+        entering={
+          checkOddEven(index) == 'odd'
+            ? BounceInLeft.duration(2000).delay(item * 8000)
+            : BounceInRight.duration(2000).delay(item * 8000)
+        }
+      />
+    );
+  };
 
   return (
     <View style={styles.mainContainer}>
-      <Ring delay={0} duration={duration} />
-      <Ring delay={1000} duration={duration} />
-      <Ring delay={2000} duration={duration} />
-      <Ring delay={3000} duration={duration} />
+      <FlatList
+        data={data}
+        keyExtractor={item => item.id}
+        onViewableItemsChanged={() => {}}
+        renderItem={({index, item}) => renderItem(index, item)}
+      />
     </View>
   );
 };
@@ -18,9 +97,7 @@ const App = () => {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
+    marginTop: 40,
   },
 });
 
